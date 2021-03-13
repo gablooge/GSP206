b43ebedf00772cf9d91577bdb1d984939be607d5
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air-2.local>
2021-03-13 10:16:50 +0700

    start from home

--- cloudshell.sh
+++ cloudshell.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~/
+
 wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
 unzip terraform_0.14.7_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
